3b6053eb2013db4f52a9a9484242f52e72974c00
Henrique Salvaro <henriquesalvaro@gmail.com>
2021-10-22 17:44:42 -0300

    fix: Add missing trailing slash to accounts URLs

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,5 @@ from django.urls import re_path, include
 
 
 urlpatterns = [
-    re_path(r"^api/v1", include("accounts.api.v1.urls")),
+    re_path(r"^api/v1/", include("accounts.api.v1.urls")),
 ]
