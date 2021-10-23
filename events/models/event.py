a96eab8f6d43685e86a3009159a728c1c5473c9b
Henrique Salvaro <henriquesalvaro@gmail.com>
2021-10-23 13:26:44 -0300

    feat: Add Event payload validation

--- events/models/event.py
+++ events/models/event.py
@@ -2,8 +2,9 @@
 Events - Event model
 """
 from django.db import models
-from django.db.models.constraints import UniqueConstraint
+from jsonschema import validate, ValidationError
 
+from events.models.event_payload import EventPayload
 from helpers.models import UUIDPrimaryKeyModel
 
 
@@ -29,3 +30,31 @@ class Event(UUIDPrimaryKeyModel):
     timestamp = models.DateTimeField(db_index=True)
     received = models.DateTimeField()
     processed = models.DateTimeField()
+
+    class InvalidPayload(BaseException):
+        pass
+
+    class NoPayloadDefinition(BaseException):
+        pass
+
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        try:
+            event_payload = EventPayload.objects.get(
+                category=self.category,
+                name=self.name,
+                applications=self.application,
+            )
+        except EventPayload.DoesNotExist:
+            raise Event.NoPayloadDefinition(
+                f"There's no payload definition for this event. Category: {self.category}. "
+                f"Name: {self.name}. Application: {self.application}."
+            )
+
+        try:
+            validate(self.data, event_payload.schema)
+        except ValidationError as exc:
+            raise Event.InvalidPayload(f"Invalid event payload. Error: {exc}")
+
+        return super().save(force_insert, force_update, using, update_fields)
