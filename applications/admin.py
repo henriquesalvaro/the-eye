1db3273200af39e7f7bcd588d5436050dc88d13f
Henrique Salvaro <henriquesalvaro@gmail.com>
2021-10-23 11:16:00 -0300

    feat: Add Application and ApplicationKey to Django Admin

--- applications/admin.py
+++ applications/admin.py
@@ -1,3 +1,22 @@
+"""
+Applications admin
+"""
 from django.contrib import admin
 
-# Register your models here.
+from applications.models import ApplicationKey, Application
+
+
+class ApplicationKeyInline(admin.TabularInline):
+    model = ApplicationKey
+    fields = ("id", "key", "revoked")
+    readonly_fields = ("id", "key")
+    extra = 0
+
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+    )
+    inlines = [ApplicationKeyInline]
