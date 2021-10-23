de0729cf9b3ea1c4d0d466730aa613343989a85f
Henrique Salvaro <henriquesalvaro@gmail.com>
2021-10-23 12:46:18 -0300

    chore: Add str methods to Application, EventCategory and EventName

--- events/models/event_category.py
+++ events/models/event_category.py
@@ -8,3 +8,9 @@ from helpers.models import UUIDPrimaryKeyModel
 
 class EventCategory(UUIDPrimaryKeyModel):
     name = models.CharField(max_length=150, unique=True, db_index=True)
+
+    class Meta:
+        verbose_name_plural = "Event categories"
+
+    def __str__(self):
+        return self.name
--- events/models/event_name.py
+++ events/models/event_name.py
@@ -8,3 +8,6 @@ from helpers.models import UUIDPrimaryKeyModel
 
 class EventName(UUIDPrimaryKeyModel):
     name = models.CharField(max_length=150, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
