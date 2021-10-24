4742518d3fc7c34ae927a01a055ccbf3559c10bc
Henrique Salvaro <henriquesalvaro@gmail.com>
2021-10-24 15:44:43 -0300

    feat: Add QueryEventsViewSet

--- events/api/v1/urls.py
+++ events/api/v1/urls.py
@@ -5,11 +5,14 @@ Events - API V1 URLs
 from django.conf.urls import url, include
 from rest_framework_nested import routers
 
-from events.api.v1.views import CreateEventViewSet
-
+from events.api.v1.views import (
+    CreateEventViewSet,
+    QueryEventsViewSet,
+)
 
 router = routers.SimpleRouter()
 router.register(r"events", CreateEventViewSet, basename="create_event")
+router.register(r"query-events", QueryEventsViewSet, basename="query_events")
 
 
 urlpatterns = [
